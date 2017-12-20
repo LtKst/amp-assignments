b17629f0a8974296e7aca640a11328f22c3a5f54
Koen Sparreboom <koensparreboom@gmail.com>
2017-12-20 09:05:01 +0100

    Finished perpendicular lines assignment

--- lines/perpendicular-lines/script.js
+++ lines/perpendicular-lines/script.js
@@ -5,6 +5,7 @@ var A = new Point(canvas, context, 100, 50, 10, "#0000FF");
 var B = new Point(canvas, context, 300, 150, 10, "#0000FF");
 
 var C = new Point(canvas, context, 200, 50, 10, "#0000FF");
+var D = new Point(canvas, context, 0, 0, 10, "#FF0000");
 
 var line1 = new Line(canvas, context, -0.3333, -133.33);
 var line2 = new Line(canvas, context, -0.3333, -133.33);
@@ -16,9 +17,33 @@ function update() {
 
   line1.drawLineByTwoPoints(A, B);
 
-  var res = line1.checkLineIntersection(A.x, A.y, B.x, B.y, A.x, A.y, B.x, B.y);
+  var dPos = getSpPoint(A, B, C);
+
+  D.x = dPos.x;
+  D.y = dPos.y;
+
+  line2.drawLineByTwoPoints(C, D);
 
   A.draw();
   B.draw();
   C.draw();
+}
+
+function getSpPoint(A, B, C) {
+  var x1 = A.x,
+    y1 = A.y,
+    x2 = B.x,
+    y2 = B.y,
+    x3 = C.x,
+    y3 = C.y;
+  var px = x2 - x1,
+    py = y2 - y1,
+    dAB = px * px + py * py;
+  var u = ((x3 - x1) * px + (y3 - y1) * py) / dAB;
+  var x = x1 + u * px,
+    y = y1 + u * py;
+  return {
+    x: x,
+    y: y
+  };
 }
