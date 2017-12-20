28efcab038ee6cca820a6c706f6a87a8a03fd7b1
Koen Sparreboom <koensparreboom@gmail.com>
2017-12-20 13:11:11 +0100

    Made draw the line points blue

--- lines/draw-the-line/script.js
+++ lines/draw-the-line/script.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-var A = new Point(canvas, context, 100, 50, 10, "#FF0000");
-var B = new Point(canvas, context, 300, 150, 10, "#FF0000");
+var A = new Point(canvas, context, 100, 50, 10, "#0000FF");
+var B = new Point(canvas, context, 300, 150, 10, "#0000FF");
 
 var line = new Line(canvas, context, -0.3333,-133.33);
 
